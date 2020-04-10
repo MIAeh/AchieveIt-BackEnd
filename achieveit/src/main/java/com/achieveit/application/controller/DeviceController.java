bd1384921be49ef4ae37274bace4d21c792788a7
Felix2048 <infinitesamsarax@outlook.com>
2020-04-11 01:16:50 +0800

    Update device: add member role authorization

--- achieveit/src/main/java/com/achieveit/application/controller/DeviceController.java
+++ achieveit/src/main/java/com/achieveit/application/controller/DeviceController.java
@@ -3,11 +3,14 @@ package com.achieveit.application.controller;
 import com.achieveit.application.annotation.Logged;
 import com.achieveit.application.entity.DeviceEntity;
 import com.achieveit.application.entity.DeviceInfo;
+import com.achieveit.application.enums.ErrorCode;
+import com.achieveit.application.exception.AchieveitException;
 import com.achieveit.application.service.DeviceService;
 import com.achieveit.application.wrapper.ResponseResult;
 import com.achieveit.application.wrapper.ResultGenerator;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -42,12 +45,12 @@ public class DeviceController {
     }
 
     @CrossOrigin
-    @Logged({"projectID", "userID", "deviceID", "dueDate"})
+    @Logged({"projectID", "userID", "deviceID", "dueDate", "session"})
     @PostMapping("/registerDevice")
-    public ResponseResult registerDevice(@RequestParam("projectID") String projectID, @RequestParam("userID") String userID, @RequestParam("deviceID") String deviceID, @RequestParam("dueDate") String dueDate) {
+    public ResponseResult registerDevice(@RequestParam("projectID") String projectID, @RequestParam("userID") String userID, @RequestParam("deviceID") String deviceID, @RequestParam("dueDate") String dueDate, HttpSession session) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            deviceService.registerDevice(projectID, userID, deviceID, dateFormat.parse(dueDate));
+            deviceService.registerDevice(projectID, userID, deviceID, dateFormat.parse(dueDate), session);
             return ResultGenerator.success();
         } catch (ParseException e) {
             e.printStackTrace();
@@ -56,10 +59,10 @@ public class DeviceController {
     }
 
     @CrossOrigin
-    @Logged({"projectID", "userID", "deviceID"})
+    @Logged({"projectID", "userID", "deviceID", "session"})
     @PostMapping("/returnDevice")
-    public ResponseResult returnDevice(@RequestParam("projectID") String projectID, @RequestParam("userID") String userID, @RequestParam("deviceID") String deviceID) {
-        deviceService.returnDevice(projectID, userID, deviceID);
+    public ResponseResult returnDevice(@RequestParam("projectID") String projectID, @RequestParam("userID") String userID, @RequestParam("deviceID") String deviceID, HttpSession session) {
+        deviceService.returnDevice(projectID, userID, deviceID, session);
         return ResultGenerator.success();
     }
 }
